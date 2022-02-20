9c466cfe573f43e96aae5d36230268ea0e0ab4f0
dinesh24murali <dinesh24murali@gmail.com>
2022-02-20 06:39:57 +0530

    Remove: salary;

---
+++ prisma/migrations/20220220010939_remove_salary/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `salary` on the `User` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "User" DROP COLUMN "salary";
